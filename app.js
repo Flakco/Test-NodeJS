2a8e0aeb2e96fc48fb7f5b56b2fb640f3fb32f2f
Flakco <flacobauhaus@gmail.com>
2023-06-28 00:38:46 -0300

    Module exercise

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 
 const { Wolf, Merry } = require('./4-names');
 const sayHi = require('./5-utils');
+const data = require('./6-other');
 
+console.log(data.singlePerson.name)
 console.log(Wolf,Merry)
 
 sayHi(Wolf)
