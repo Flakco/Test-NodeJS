c6c81adc4ba55bbbcb2e42d5588dc2f67f6b46d7
Flakco <flacobauhaus@gmail.com>
2023-07-01 17:53:13 -0300

    Event loop examples

---
+++ a.js
@@ -0,0 +1,18 @@
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+console.log("Inicio del programa");
+
+// Ejemplo de una tarea asíncrona: setTimeout
+setTimeout(() => {
+  console.log("Tarea asíncrona completada");
+}, 0);
+
+// Otra tarea síncrona
+console.log("Tarea síncrona");
+
+console.log("Fin del programa");
