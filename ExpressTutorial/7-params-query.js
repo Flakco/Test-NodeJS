06d6f512bb9671ee536f1fcc61ea382ea6afa0f9
Flakco <flacobauhaus@gmail.com>
2023-07-06 18:34:50 -0300

    Express Tutorial

--- ExpressTutorial/7-params-query.js
+++ ExpressTutorial/7-params-query.js
@@ -18,9 +18,8 @@ app.get('/api/products/:productID', (req, res) => {
   // console.log(req.params)
   const { productID } = req.params
 
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
+  const singleProduct = products.find((product) => product.id === Number(productID))
+  
   if (!singleProduct) {
     return res.status(404).send('Product Does Not Exist')
   }
